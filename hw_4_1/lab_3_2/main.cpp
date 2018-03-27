8c2f4158b610f727d8f1a7a5067ef38ff9a5683e
KimSeongJun <gmkseta@gmail.com>
2018-03-28 03:47:30 +0900

    일단 2번 완료 맞는걸까?

---
+++ hw_4_1/lab_3_2/main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+typedef int itemType;
+
+void shuffle(itemType* arr, int n)
+{
+	//srand((unsigned int)time(NULL));
+
+	for (int i = 1; i <= n; i++)
+	{
+		int a = rand() % n + 1;
+
+		itemType tmp = arr[a];
+		arr[a] = arr[i];
+		arr[i] = tmp;
+	}
+}
+
+
+void insertSort(itemType a[], int n)
+{
+	int i, j; itemType v;
+	
+	for (i = 2; i <= n; i++)
+	{
+		v = a[i]; j = i;
+
+
+		while (a[j - 1] > v)
+		{
+			a[j] = a[j - 1];
+			j--;
+			a[j] = v;
+		}
+	}
+}
+
+std::string shellSort(itemType a[],int n)
+{
+
+	int i, j, h; itemType v;
+	int Compare_Cnt = 0, DataMove_Cnt = 0;
+
+	h = 1;  do h = 3 * h + 1; while (h < n);
+	do {
+		h = h / 3;
+		for (i = h; i <= n; i++)
+		{
+			v = a[i]; j = i;
+			while (++Compare_Cnt&& a[j - h] > v)
+			{
+				a[j] = a[j - h]; j -= h; 
+				++DataMove_Cnt;
+				if (++Compare_Cnt && j <= h - 1) break;
+			}
+			a[j] = v;
+		}
+	} while (h > 1);
+
+
+	std::string result = "Compare_Cnt : " + std::to_string(Compare_Cnt) + " DataMove_Cnt : " + std::to_string(DataMove_Cnt);
+	return result;
+
+}
+
+
+int main()
+{
+
+	using namespace std;
+	int n;
+
+	cout << "배열의 크기를 입력하세요 (10000이상) >>";
+	cin >> n;
+
+	itemType *des_ord = new itemType[n + 1];
+	itemType *rand_ord = new itemType[n + 1];
+
+	for (int i = 1; i <= n; i++)
+	{
+		des_ord[i] = n - i + 1;
+		rand_ord[i] = n - i + 1;
+	}
+	des_ord[0] = INT_MIN;
+	rand_ord[0] = INT_MIN;
+
+	shuffle(rand_ord, n);
+
+
+	cout << "Descending Order Array" << endl;
+	for (int i = 1; i <= 20; i++)
+		cout << des_ord[i] << " ";
+
+	cout << endl << shellSort(des_ord, n) << endl;
+
+
+
+	cout << "Random Order Array" << endl;
+	for (int i = 1; i <= 20; i++)
+		cout << rand_ord[i] << " ";
+	cout << endl <<  shellSort(rand_ord, n) << endl;
+
+
+
+}
