a5fd3597cd6e7649a052cc750703619b0e0fc69f
KimSeongJun <gmkseta@gmail.com>
2018-04-15 03:02:50 +0900

    그냥 만들어 봤는데 맞는듯..? 랜덤한 어레이 만드는 클래스 하나해서 그 인스턴스에서 받아온 값으로 넣고 돌림.

--- lab/lab6/lab6_1/main.cpp
+++ lab/lab6/lab6_1/main.cpp
@@ -1,7 +1,11 @@
-﻿#define infoNIL 0
+﻿#include <iostream>
+#include <ctime>
+
+#define infoNIL 0
 #define itemMIN -1
 typedef int itemType;
 typedef double infoType;
+int compare;
 
 class BST {
 private:
@@ -19,25 +23,74 @@ public:
 		z = new node(0, infoNIL, 0, 0);
 		head = new node(itemMIN, infoNIL, z, z);
 	}
-	~BST();
-	infoType  BSTsearch(itemType v);
-	void BSTinsert(itemType v, infoType info);
+	~BST(){};
+	infoType BSTsearch(itemType v) {
+		struct node *x = head->r;
+		z->key = v;  // 아래 반복문을 간결히 만들기 위함
+		while (++compare&&v != x->key)  x = (v < x->key) ? x->l : x->r;
+		return x->info;
+	}
+	void BSTinsert(itemType v, infoType info) {
+		struct node *p, *x;
+		p = head; x = head->r;
+		while (x != z) { p = x; x = (v < x->key) ? x->l : x->r; }
+		x = new node(v, info, z, z);
+		if (v < p->key) p->l = x; else p->r = x;
+	}
 };
-infoType BST::BSTsearch(itemType v) {
-	struct node *x = head->r;
-	z->key = v;  // 아래 반복문을 간결히 만들기 위함
-	while (v != x->key)  x = (v < x->key) ? x->l : x->r;
-	return x->info;
-}
-void BST::BSTinsert(itemType v, infoType info) {
-	struct node *p, *x;
-	p = head; x = head->r;
-	while (x != z) { p = x; x = (v < x->key) ? x->l : x->r; }
-	x = new node(v, info, z, z);
-	if (v < p->key) p->l = x; else p->r = x;
-}
+
+
+class makeArr
+{
+private:
+	itemType* arr;
+	int N;
+public : 
+	
+	makeArr(int n)
+	{
+		N = n;
+		arr = new itemType[n];
+	}
+
+	void make_des_arr(){for (int i = 0; i < N; i++) arr[i] = N - i;}
+	void make_rand_arr() 
+	{
+		srand((unsigned int)time(NULL));
+		make_des_arr(); 
+		for (int i = 0; i < N; i++)
+		{
+			int c = rand() % N;
+			itemType tmp = arr[c];
+			arr[c] = arr[i]; arr[i] = tmp;
+		}
+	}
+	itemType get(int n) { return arr[n]; }
+};
 
 int main()
 {
+	using namespace std;
+	extern int compare;
+	compare = 0;
+	int n;
+	cout << "배열의 크기를 입력하세요 (10000)" << endl;
+	cin >> n;
+
+	makeArr arr(n);
+	BST tree(n);
+
+	arr.make_rand_arr();
+
+	for (int i = 0; i < n; i++)
+		tree.BSTinsert(arr.get(i), infoNIL);
+
+	for (int i = 0; i < n; i++)
+		tree.BSTsearch(i);
+
+	cout << ((float)compare/n) << endl;
+
+	
 	return 0;
 }
+
