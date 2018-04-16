52c7b1595c2b02959b6f2a6dc884623d09a65489
KimSeongJun <gmkseta@gmail.com>
2018-04-17 01:19:56 +0900

    MakeHeap에 주석 달고 공부하느라 알게된 datamove_cnt 와 compare_cnt 수정

--- lab/lab_5/lab_5/main.cpp
+++ lab/lab_5/lab_5/main.cpp
@@ -2,10 +2,10 @@
 #include <ctime>
 #include <cstdlib>
 
-
 typedef int itemType;
 
 int compare = 0, datamove = 0;
+
 class MAKE_HEAP {
 private:
 	itemType *a;
@@ -23,33 +23,37 @@ public:
 		int Parent, LeftSon, RightSon, Son; itemType RootValue;
 		Parent = Root;
 		RootValue = a[Root];
-		LeftSon = 2 * Parent + 1;
-		RightSon = LeftSon + 1;
-		while (++compare&&LeftSon <= LastNode){
+		LeftSon = 2 * Parent + 1;//이런방식으로 하면 root가 0이여도 자식 잘 갈수있
+		RightSon = LeftSon + 1;//원래는 2*Parent 가 Left , Right 2*Parent+1 자나
+		while (LeftSon <= LastNode){
+			//반목문이 LastNode 와 LeftSon을 비교하면서 돈다.
+			//LeftSon이 더 커지는순간 나와짐. 즉 다돌면..
 			if (++compare&&RightSon <= LastNode && a[LeftSon] < a[RightSon]) {
-				Son = RightSon;
+				Son = RightSon;//Right 가 더크면 Right 넣고
 			}
-			else Son = LeftSon;
-			datamove++;
-			if (++compare&&RootValue < a[Son]) {
+			else Son = LeftSon;//left가 더 크면 left 를 넣음 즉 더 큰것을 넣음
+			if (++compare&&RootValue < a[Son]) {//Root가 더 작으면? (바꿔줘야지!)
 				datamove++;
-				a[Parent] = a[Son];
-				Parent = Son;
-				LeftSon = Parent * 2 + 1;
-				RightSon = LeftSon + 1;
-				
-			}
+				a[Parent] = a[Son];//Root에 넣어주고 더 큰 값인 Son을 넣어주고
+				Parent = Son;//Parent index를 Son으로 바꾸고
+				LeftSon = Parent * 2 + 1;//그녀석의 왼쪽자식
+				RightSon = LeftSon + 1;	//오른쪽 자식하고 비교
+			}//DownHeap 삭제 연산을 하는과정임.
 			else break;
-		}
-		a[Parent] = RootValue;
+		}//이거로 힙을 다 바꾸고 . 
+		a[Parent] = RootValue; //루트벨류의 최종위치에 넣어줌.
 		datamove++;
 	}
 	void heapsort() {
 		int i;
 		compare = 0; datamove = 0;
-		
-		for (i = N / 2; i >= 1; i--)  MakeHeap(a, i - 1, N - 1);
-		for (i = N - 1; i >= 1; i--) { swap(a, 0, i); MakeHeap(a, 0, i - 1); }
+		for (i = N / 2; i >= 1; i--) 
+			MakeHeap(a, i - 1, N - 1);
+		for (i = N - 1; i >= 1; i--) 
+		{ 
+			swap(a, 0, i); 
+			MakeHeap(a, 0, i - 1); 
+		}
 	}
 
 	void make_rand_arr() 
